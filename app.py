10457e9409f79cd18104e75508265d0c32a32290
Logan Cox <195850+lo5an@users.noreply.github.com>
2023-08-02 00:46:12 -0500

    Fixup comments and whitespace

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ def update_combined_events(event):
     libcal_oauth_token = get_oauth_token(creds)
 
     all_events = get_combined_events(libcal_oauth_token)
-
     if len(all_events) > 2:
         write_combined_events({"events": all_events[0:2]})
     else:
@@ -58,7 +57,7 @@ def get_combined_events(libcal_oauth_token):
     #  12025     LL118
     #  12221     LL121E
     #  12023     LL123
-
+    #
     # For all of the above, we're querying two categories of event
     # - OU Libraries Event
     # - Research Wrokshops
@@ -77,11 +76,9 @@ def get_combined_events(libcal_oauth_token):
         events_resp = requests.get(cal, headers=headers, timeout=5)
         events_json = events_resp.json()
         all_events.extend(events_json["events"])
-
     # Sort by start time could be weird for long running events...
     # Going with simplest solution until we can prove that we don't need something better.
     sorted(all_events, key=lambda event: event["start"])
-
     return all_events
 
 
@@ -105,14 +102,13 @@ def write_combined_events(events_json):
 
 
 def get_secret():
-    """Get JSON blob of secrets required to atuh with libcal API from AWS Secrets Manager."""
+    """Get JSON blob of secrets required to auth with libcal API from AWS Secrets Manager."""
     secret_name = "prod/lambdaCal/LibcalEventsRO"
     region_name = "us-east-1"
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=region_name)
-
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
@@ -120,15 +116,15 @@ def get_secret():
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         raise e
 
-    # Decrypts secret using the associated KMS key.
+    # Retrieve decrypted secret using the associated KMS key.
     secret = get_secret_value_response["SecretString"]
-
     return json.loads(secret)
 
 
 # Get Oauth token
 def get_oauth_token(creds):
     """Get oauth token from libcal using Client Credentials Flow"""
+
     client_id = creds["client_id"]
     client_secret = creds["secret"]
     client = BackendApplicationClient(client_id=client_id)
